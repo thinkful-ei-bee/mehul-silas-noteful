d52ce442ec5bbc4bf00f0581a3a7bb84263ecdd6
Mehul Patel <mehulpatel118@gmail.com>
2019-03-26 16:47:54 -0400

    finished

--- src/Folder.js
+++ src/Folder.js
@@ -7,7 +7,6 @@ class Folder extends React.Component  {
 
   
   render(){
-    console.log(this.props.changeOrigin);
     return ( 
       <li key={this.props.folderid} className ='folderItem'>         
         <NavLink onClick={() =>this.props.changeOrigin(false)} to={`/Folder/${this.props.folderid}`}>
